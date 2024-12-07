ddf935b3f4fbf967127bddb80849cd70ae871147
Андрей <andrey.ustas1@gmail.com>
2024-12-07 23:40:43 +0500

    07/12

---
+++ practic_work.py
@@ -0,0 +1,16 @@
+def trap(number):
+    password = ''
+    for i in range(1, number):
+        for j in range(2, number):
+            if j <= i:
+                continue
+            if number % (i + j) == 0:
+                password += str(i) +str(j)
+    return password
+n = int(input('Введите число от 3 до 20: '))
+
+result = trap(n)
+print("Пароль", result)
+
+
+
