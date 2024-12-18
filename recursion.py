2242fcc1c3ee132892019caca79422f59fc17038
Андрей <andrey.ustas1@gmail.com>
2024-12-18 23:45:32 +0500

    16/12

--- recursion.py
+++ recursion.py
@@ -3,8 +3,11 @@ def get_multiplied_digits (number):
     first = int (str_number [0])
     if len(str(number)) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    return first
+    else:
+        return first if first !=0 else 1
 
 
-result = get_multiplied_digits(42608)
+result = get_multiplied_digits(40203)
 print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
