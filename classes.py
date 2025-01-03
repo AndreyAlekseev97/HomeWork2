8b4a8d1dc38320f264ba809806c8c9de29612f1d
Андрей <andrey.ustas1@gmail.com>
2025-01-03 23:41:35 +0500

    03/01

--- classes.py
+++ classes.py
@@ -2,10 +2,10 @@ class House:
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-        self.info()
+        # self.info()
 
-    def info(self):
-        print(f'Наименование: {self.name}. Этаж: {self.number_of_floors}')
+    # def info(self):
+    #     print(f'Наименование: {self.name}. Этаж: {self.number_of_floors}')
 
     def go_to(self, new_floor):
         self.new_floor = new_floor
@@ -18,7 +18,20 @@ class House:
                 break
         return self.new_floor
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название : {self.name}, количество этажей: {self.number_of_floors}'
+
+
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
 h1.go_to(16)
 h2.go_to(5)
+
+print(h1)
+print(h2)
+
+print(len(h1))
+print(len(h2))
